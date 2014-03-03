7813ddc326c230062587b0fc74379f333dd15f69
Benjamin Gröhbiel <benjamin.groehbiel@gmail.com>
2014-03-03 17:35:47 +0100

    String compression draft.

---
+++ Chapt1/src/StringCompressor.java
@@ -0,0 +1,11 @@
+/*
+ * Method which compresses aabccccaaa into a2b1c4a3. 
+ * If compressed string is not shorter than original, output original.
+ */
+public class StringCompressor {
+	String compress(String s) {
+		char[] compressed = new char[s.length()];
+		
+		return null;
+	}
+}
